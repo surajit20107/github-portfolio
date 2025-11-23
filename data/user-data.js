2c276777edbdfe2e85c7d0c778a7f231de7a28c4
Surajit <surajitjana20107@gmail.com>
2025-11-23 21:59:24 +0530

    Update Stack Overflow link in user data

--- data/user-data.js
+++ data/user-data.js
@@ -5,7 +5,7 @@ export const userData = {
   facebook: 'https://www.facebook.com/surajit20107',
   linkedIn: 'https://www.linkedin.com/in/surajit-jana20107',
   twitter: 'https://twitter.com/surajit_20107',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  stackOverflow: 'https://stackoverflow.com/users/16848562/surajit',
   leetcode: "https://leetcode.com/surajit_07",
   resume: "https://drive.google.com/file/d/1W-N81Cp-Lp-bN5MnkS-d-vTEfukbUiew/view?usp=drivesdk",
   skills: ['React', 'NextJS', 'Redux', 'Express', 'Node.js', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
