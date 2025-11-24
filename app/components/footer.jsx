dbdc383d1d4e7c03db8c8853579698097e374bb3
Surajit <surajitjana20107@gmail.com>
2025-11-24 09:18:23 +0530

    Update name in footer component

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © GitHub Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/surajit-jana20107" className="text-primary-icon">Abu Said</Link>
+            © GitHub Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/surajit-jana20107" className="text-primary-icon">Surajit</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
